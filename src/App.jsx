c67d21259e9c3a9611503e7d338080fa5a6b00f4
AbdulMoeez21 <moizrao7@gmail.com>
2023-03-16 01:14:37 +0500

    ncomplete -_- api isn't working

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import Header from "./components/ui/Header";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`
+  https://www.breakingbadapi.com/api/characters`);
+      console.log(result.data);
+    };
+    fetchItems()
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
